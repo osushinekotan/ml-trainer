d90fa0446665ae2c59f2922dab7ff3bb64f51261
osushinekotan <spmt3710@gmail.com>
2024-02-25 18:19:47 +0900

    Add unit tests for tabular utils module

---
+++ tests/tabular/utils/test_utils.py
@@ -0,0 +1,38 @@
+import pytest
+
+from src.ml_trainer.tabular.utils.utils import generate_uid
+
+
+@pytest.mark.parametrize(
+    "args",
+    [
+        ("test",),  # from string
+        (1,),  # from int
+        (1, 2, 3),  # from multiple args
+        ({"test": 1},),  # from dict
+        (["test", 1],),  # from list
+        (lambda x="dummy": x,),  # from function
+    ],
+)
+def test_can_generate_uid(args):  # noqa
+    uid = generate_uid(*args)
+    assert isinstance(uid, str)
+
+
+@pytest.mark.parametrize(
+    "args",
+    [
+        ("test",),
+        (1,),
+        (1, 2, 3),
+        ({"test": 1},),
+        (["test", 1],),
+        (lambda x="dummy": x,),
+    ],
+)
+def test_generate_uid_is_consistent(args):  # noqa
+    uid1 = generate_uid(*args)
+    uid2 = generate_uid(*args)
+
+    assert uid1 == uid2, "UIDs generated from the same inputs should be identical"
+    print(uid1)
