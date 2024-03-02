aef2e9edc55eb8de1030ce9cd7133e3cd1171696
osushinekotan <spmt3710@gmail.com>
2024-03-02 14:10:11 +0900

    Update

--- src/ml_trainer/tabular/trainer.py
+++ src/ml_trainer/tabular/trainer.py
@@ -2,10 +2,14 @@
 import json
 from pathlib import Path
 
+import japanize_matplotlib
 import joblib
+import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 import polars as pl
+import seaborn as sns
+from matplotlib.figure import Figure
 from numpy.typing import ArrayLike
 from rich.console import Console
 from sklearn.metrics import mean_absolute_error, mean_absolute_percentage_error, roc_auc_score
@@ -54,9 +58,12 @@ class Trainer:
         self.n_splits = n_splits
         self.groups = groups
         self.seed = seed
+        self.eval_metrics = eval_metrics
+        self.custom_eval = custom_eval
 
         self.kwargs = kwargs
         self.is_fitted = False
+        self.is_cv = False
 
     def get_eval_metrics(self, task: str) -> dict[str, callable]:
         if self.eval_metrics is None:
@@ -105,9 +112,7 @@ class Trainer:
         Returns:
             dict: 学習結果を格納した辞書。 {estimator_uid: {"estimator": estimator, "pred": pred, "scores": scores}} の形式で出力される。
         """
-        if out_dir is not None:
-            out_dir = self.out_dir
-
+        out_dir = out_dir or self.out_dir
         out_dir.mkdir(exist_ok=True, parents=True)
 
         task = self.judge_task(y_train)
@@ -158,7 +163,7 @@ class Trainer:
         self.is_fitted = True
         return resutls
 
-    def cv(self, X_train: XyArrayLike, y_train: XyArrayLike) -> dict[str, ArrayLike]:
+    def train_cv(self, X_train: XyArrayLike, y_train: XyArrayLike) -> dict[str, ArrayLike]:
         """Cross Validation.
 
         Args:
@@ -168,6 +173,8 @@ class Trainer:
         Returns:
             dict[str, ArrayLike]: estimator ごとの oof 予測値
         """
+        self.is_cv = True
+
         if isinstance(X_train, pd.DataFrame) or isinstance(X_train, pl.DataFrame):
             # NOTE : split_type が "fold" かつ X_train が DataFrame の場合は fold カラムに従って分割する splitter を作成
             if self.split_type == "fold" and "fold" in X_train.columns:
@@ -185,6 +192,8 @@ class Trainer:
             ).split(X_train, y_train, groups=self.groups)
 
         task = self.judge_task(y_train)
+        console.print(f"[{task}] Cross Validation: {self.n_splits} folds", style="bold green")
+
         metrics = self.get_eval_metrics(task)
 
         fold_fitted_results = {}
@@ -231,9 +240,9 @@ class Trainer:
                     style="bold blue",
                 )
                 if est not in oof_results:
-                    oof_results[est] = data["pred"]
+                    oof_results[est] = list(data["pred"])
                 else:
-                    oof_results[est].extend(data["pred"])
+                    oof_results[est].extend(list(data["pred"]))
 
         return oof_results
 
@@ -273,17 +282,193 @@ class Trainer:
 
         return resutls
 
-    def get_feature_importance(self):
-        pass
+    def predict_cv(self, X: XyArrayLike, out_dir: Path | None = None, save: bool = False) -> dict[str, ArrayLike]:
+        """Predict. (複数の) Estimator を使って予測を行う。
 
-    def plot_feature_importance(self):
-        pass
+        Args:
+            X (XyArrayLike): 予測用の特徴量
+            out_dir (Path | None, optional): 学習ずみの Estimator が保存されているディレクトリ. Defaults to None.
+            save (bool, optional): 予測結果を out_dir に保存するかどうか. Defaults to False.
 
-    def save(self):
-        pass
+        Returns:
+            dict[str, ArrayLike]: estimator ごとの予測値を格納した辞書。fold ごとの平均予測値が出力される。
+        """
+        console.print(f"Predict Cross Validation : {self.n_splits} folds.", style="bold green")
 
-    def load(self):
-        pass
+        fold_results = {}
+        for i_fold in range(self.n_splits):
+            fold_dir = out_dir / f"fold{i_fold}"
+
+            pred_results = self.predict(X, out_dir=fold_dir, save=False)  # NOTE : cv 予測時は各 fold pred は保存しない
+            fold_results[f"fold{i_fold}"] = pred_results
+
+        fold_means = self.get_fold_mean(fold_results)
+        if save:
+            for est, pred in fold_means.items():
+                result_dir = out_dir / "results" / est
+                result_dir.mkdir(exist_ok=True, parents=True)
+                joblib.dump(pred, result_dir / "test_pred.pkl")
+        return fold_means
+
+    def get_fold_mean(self, fold_results: dict[str, ArrayLike]) -> dict[str, ArrayLike]:
+        """Get Aggregated Prediction results.
+
+        Returns:
+            dict: estimator ごとの fold ごとの予測値を集約した辞書。 {est1: pred, est2: pred, ...} の形式で出力される (pred: ArrayLike)。
+        """
+        # 各 est の fold ごとの pred 値を収集
+        fold_values = {est: [] for est in fold_results[fold_results.keys()][0]}
+        for fold in fold_results:
+            for est, data in fold_results[fold].items():
+                fold_values[est].append(data["pred"])
+
+        # 各 est の fold 平均を計算
+        fold_means = {}
+        for est, values in fold_values.items():
+            mean_values = np.mean(values, axis=0).tolist()  # mean 予測値
+            fold_means[est] = mean_values
+
+        return fold_means
+
+    def make_plot_feature_importances(
+        self,
+        out_dir: Path | None = None,
+        top_n: int | None = 50,
+        save: bool = True,
+    ) -> dict[str, pd.DataFrame]:
+        """Get Feature Importance.
+
+        Args:
+            out_dir (Path | None, optional): 学習ずみの Estimator が保存されているディレクトリ. Defaults to None.
+            top_n (int | None, optional): 上位何個の feature importance をプロットするか。None なら全てプロットする。 Defaults to None.
+            save (bool, optional): 予測結果を out_dir に保存するかどうか. Defaults to True.
+
+        Returns:
+            dict[str, pd.DataFrame]:
+                estimator ごとの feature importance を格納した辞書。 {est1: importance_df, est2: importance_df, ...} の形式で出力される。
+                importance_df は feature_name, importance, fold のカラムを持つ DataFrame。
+        """
+        if not self.is_fitted:
+            raise ValueError("Estimator is not fitted yet.")
+
+        out_dir = out_dir or self.out_dir
+        n_splits = self.n_splits if self.is_cv else 1
+
+        feature_importances = {est.uid: pd.DataFrame() for est in self.estimators}
+        for i_fold in range(n_splits):
+            if not self.is_cv:
+                result_dir = out_dir
+            else:
+                result_dir = out_dir / f"fold{i_fold}"
+
+            for estimator in self.estimators:
+                estimator_uid = estimator.uid
+                estimator_dir = result_dir / estimator_uid
+                estimator.load(estimator_dir / "estimator.pkl")
+
+                importance_df = estimator.get_feature_importance()
+                importance_df["fold"] = i_fold  # not is_cv なら fold は 0 にする
+
+                feature_importances[estimator_uid] = pd.concat(
+                    [feature_importances[estimator_uid], importance_df], axis=0
+                )
+
+        # NOTE : plot feature importance
+        for est, df in feature_importances.items():
+            fig = self.make_feature_importance_fig(df, plot_type="auto", top_n=top_n)
+            fig.show()
+            if save:
+                save_dir = out_dir / "results" / est
+                df.to_csv(save_dir / "feature_importance.csv", index=False)
+                fig.savefig(save_dir / "feature_importance.png", dpi=300)
+
+        return feature_importances
+
+    def make_feature_importance_fig(
+        self,
+        feature_importance_df: pd.DataFrame,
+        plot_type: str = "auto",
+        top_n: int | None = None,
+    ) -> Figure | pd.DataFrame:
+        japanize_matplotlib.japanize()
+
+        if plot_type == "auto":
+            # NOTE : fold ユニーク数が 1 なら bar, 2 以上なら boxen
+            plot_type = "boxen" if feature_importance_df["fold"].nunique() > 1 else "bar"
+
+        if plot_type not in ["boxen", "bar"]:
+            raise ValueError("Invalid plot_type")
+
+        order = (
+            feature_importance_df.groupby("feature")
+            .sum()[["importance"]]
+            .sort_values("importance", ascending=False)
+            .index
+        )
+        if top_n is not None:
+            order = order[:top_n] or order
+
+        fig, ax = plt.subplots(figsize=(12, max(6, len(order) * 0.25)))
+        plot_params = dict(
+            data=feature_importance_df,
+            x="importance",
+            y="feature",
+            order=order,
+            ax=ax,
+            palette="viridis",
+            orient="h",
+        )
+        if plot_type == "boxen":
+            sns.boxenplot(**plot_params)
+        elif plot_type == "bar":
+            sns.barplot(**plot_params)
+        else:
+            raise NotImplementedError()
+
+        ax.tick_params(axis="x", rotation=90)
+        ax.set_title("Importance")
+        ax.grid()
+        fig.tight_layout()
+        return fig
+
+    @property
+    def snapshot_items(self):
+        return [
+            "estimators",
+            "ensemble",
+            "out_dir",
+            "feature_names",
+            "estimator_name",
+            "split_type",
+            "n_splits",
+            "groups",
+            "seed",
+            "kwargs",
+            "is_fitted",
+            "is_cv",
+            "eval_metrics",
+            "custom_eval",
+        ]
+
+    def save(self, filepath: Path) -> None:
+        """snapshot items を保存する."""
+        filepath.parent.mkdir(exist_ok=True, parents=True)
+        snapshot = tuple([getattr(self, item) for item in self.snapshot_items])
+        joblib.dump(snapshot, filepath)
+
+    @classmethod
+    def load(cls, filepath: Path) -> "Trainer":
+        """保存した snapshot items を classmethod で読み込む.
+        >>> trainer = Trainer.load("trainer.pkl")
+        >>> trainer.predict(X)
+        """
+        if not filepath.exists():
+            raise FileNotFoundError(f"{filepath} does not exist.")
+
+        snapshot = joblib.load(filepath)
+        for item, value in zip(cls.snapshot_items, snapshot):
+            setattr(cls, item, value)
+        return cls
 
     def _generate_folds(self, fold_list: list | pd.Series):
         fold_series = pd.Series(fold_list)
